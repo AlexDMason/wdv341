46972168d95bd0f64a334a2f4510e578973d2e68
AlexDMason <143130766+AlexDMason@users.noreply.github.com>
2023-08-31 13:36:33 -0500

    Add files via upload

---
+++ 2-1_php_basics.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Basics</title>
+</head>
+<body>
+
+<?php
+$yourName = "Alex Mason";
+$number1 = 1;
+$number2 = 2;
+echo "<h1>PHP Basics</h1>";
+
+echo "<h2>$yourName</h2>";
+
+echo "Number One: $number1 ";
+echo "Number Two: $number2 ";
+echo "Total: ";
+echo $number1 + $number2;
+?>
+
+<script>
+        
+        <?php
+            echo "let languages =['PHP', 'HTML', 'Javascript'];";
+            echo "for(let i=0; i < languages.length; i++){
+                    document.write(languages[i]+ ' ' );
+                    }";
+        ?>
+    
+    </script>
+    
+</body>
+</html>
